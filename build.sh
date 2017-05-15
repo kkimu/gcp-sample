ce5c8525920652c4ab3813a5bf87d07f37fa2116
kkimu <kimura.kzm@gmail.com>
2017-05-15 19:17:25 +0900

    Update

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-version=0.3.2 # imageのversionを指定
-#cd $GOPATH/src/github.com/techcampman/insta-team-4_2
-#git pull
-GOOS=linux go build -o /Users/a14730/work/instagram/instagram-infra/go-pro/main $GOPATH/src/github.com/techcampman/insta-team-4_2/main.go
-docker build -t gcr.io/cyberagent-148/go-app:$version -t gcr.io/cyberagent-148/go-app:latest go-pro
+set -ex
+version=0.4.2 # imageのversionを指定
+path=$PWD
+cd $GOPATH/src/github.com/techcampman/insta-team-4_2
+git pull
+cd $path
+GOOS=linux go build -o /Users/a14730/work/instagram/instagram-infra/go-app/main $GOPATH/src/github.com/techcampman/insta-team-4_2/main.go
+docker build -t gcr.io/cyberagent-148/go-app:$version -t gcr.io/cyberagent-148/go-app:latest go-app
 gcloud docker -- push gcr.io/cyberagent-148/go-app:$version
 gcloud docker -- push gcr.io/cyberagent-148/go-app:latest
--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-version=0.3.2 # imageのversionを指定
+version=0.4.2 # imageのversionを指定
 
 kubectl set image deployment/go-app-deployment go-app=gcr.io/cyberagent-148/go-app:$version
